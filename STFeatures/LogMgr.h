3b999120939ca820656fea5a1fe863cadbbf36a1
Buanderie <nicolas.said@gmail.com>
2011-06-14 13:51:33 +0200

    XML based input..

--- STFeatures/LogMgr.h
+++ STFeatures/LogMgr.h
@@ -1,6 +1,10 @@
 #ifndef __LOGMGR_H__
 #define __LOGMGR_H__
 
+#include <vector>
+#include <string>
+#include <ostream>
+
 #include "Singleton.h"
 
 class CLogMgr : public CSingleton< CLogMgr >
@@ -8,9 +12,13 @@ class CLogMgr : public CSingleton< CLogMgr >
 	friend class CSingleton<CLogMgr>;
 
 	private:
+		std::vector< std::string > _messageList;
 
 	public:
 		void init();
+		void addMsg( std::string sender, std::string content );
+		void flushTo( std::ostream& stream );
+
 };
 
 #endif
